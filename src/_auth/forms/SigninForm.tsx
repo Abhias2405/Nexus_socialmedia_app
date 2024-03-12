f7910e9cc5bd2b468fad81b2599be1aec944bdf0
Abhias2405 <abhias2405@gmail.com>
2024-03-13 00:12:36 +0530

    Setup

---
+++ src/_auth/forms/SigninForm.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const SigninForm = () => {
+  return (
+    <div>
+      <Button>Button</Button>
+    </div>
+  );
+};
+
+export default SigninForm;
